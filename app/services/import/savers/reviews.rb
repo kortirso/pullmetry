b330659f590950e187c01cd5a086ad80113ae4b8
Bogdanov Anton <kortirso@gmail.com>
2023-01-01 10:17:15 +0400

    add synchronization of PR reviews

---
+++ app/services/import/savers/reviews.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Import
+  module Savers
+    class Reviews
+      prepend ApplicationService
+      include Concerns::FindOrCreateEntity
+
+      def call(pull_request:, data:)
+        @pull_request = pull_request
+
+        ActiveRecord::Base.transaction do
+          destroy_old_reviews(data)
+          data.each do |payload|
+            next if payload[:external_id].in?(existing_reviews)
+
+            entity = find_or_create_entity(payload.delete(:author))
+            pr_entity = find_or_create_pr_entity(entity)
+            create_review(pr_entity, payload)
+          end
+        end
+      end
+
+      private
+
+      def destroy_old_reviews(data)
+        @pull_request
+          .pull_requests_reviews
+          .where.not(external_id: data.pluck(:external_id))
+          .destroy_all
+      end
+
+      def existing_reviews
+        @existing_reviews ||=
+          @pull_request
+          .pull_requests_reviews
+          .pluck(:external_id)
+      end
+
+      # here we create pr entity with reviewer origin because author's pr entity always exists
+      # and author can't approve his own PR
+      def find_or_create_pr_entity(entity)
+        @pull_request
+          .pull_requests_entities
+          .create_with(origin: ::PullRequests::Entity::REVIEWER)
+          .find_or_create_by!(entity_id: entity)
+      end
+
+      def create_review(pr_entity, payload)
+        pr_entity.pull_requests_reviews.create!(payload)
+      end
+    end
+  end
+end
--- app/services/import/sync_repositories_service.rb
+++ app/services/import/sync_repositories_service.rb
@@ -7,10 +7,12 @@ module Import
     def initialize(
       sync_pull_requests_service: SyncPullRequestsService.new,
       sync_comments_service: SyncCommentsService.new,
+      sync_reviews_service: SyncReviewsService.new,
       generate_insights_service: Insights::GenerateService.new
     )
       @sync_pull_requests_service = sync_pull_requests_service
       @sync_comments_service = sync_comments_service
+      @sync_reviews_service = sync_reviews_service
       @generate_insights_service = generate_insights_service
     end
 
@@ -19,6 +21,7 @@ module Import
         @sync_pull_requests_service.call(repository: repository)
         repository.pull_requests.find_each do |pull_request|
           @sync_comments_service.call(pull_request: pull_request)
+          @sync_reviews_service.call(pull_request: pull_request)
         end
         @generate_insights_service.call(insightable: repository)
       end
