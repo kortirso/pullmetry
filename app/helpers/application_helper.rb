1490813015b1656a05fd6179fde4c63fee1f2615
Bogdanov Anton <kortirso@gmail.com>
2023-01-08 16:17:35 +0400

    update omniauth credentials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def github_omniauth_link
-    "https://github.com/login/oauth/authorize?client_id=#{credentials.dig(:github_oauth, Rails.env.to_sym, :client_id)}&redirect_uri=#{credentials.dig(:github_oauth, Rails.env.to_sym, :redirect_url)}&scope=user:email"
+    "https://github.com/login/oauth/authorize?client_id=#{credentials.dig(:github_oauth, Rails.env.to_sym, :client_id)}&redirect_uri=#{credentials.dig(:github_oauth, Rails.env.to_sym, :redirect_url)}&scope=user:email&provider_ignores_state=true&response_type=code"
   end
 
   def credentials
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,19 +5,10 @@ credentials = Rails.application.credentials
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.development?
 
-  if Rails.env.production?
-    provider :github,
-             credentials.dig(:github_oauth, :production, :client_id),
-             credentials.dig(:github_oauth, :production, :client_secret),
-             scope: 'user:email',
-             redirect_url: credentials.dig(:github_oauth, :production, :redirect_url),
-             provider_ignores_state: true
-  else
-    provider :github,
-             credentials.dig(:github_oauth, :development, :client_id),
-             credentials.dig(:github_oauth, :development, :client_secret),
-             scope: 'user:email',
-             redirect_url: credentials.dig(:github_oauth, :production, :redirect_url),
-             provider_ignores_state: true
-  end
+  provider :github,
+           credentials.dig(:github_oauth, Rails.env.to_sym, :client_id),
+           credentials.dig(:github_oauth, Rails.env.to_sym, :client_secret),
+           scope: 'user:email',
+           redirect_url: credentials.dig(:github_oauth, Rails.env.to_sym, :redirect_url),
+           provider_ignores_state: true
 end
