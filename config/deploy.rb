b0a8eb52b69f98a2d40ddb3d2bc2b37a133fcfcf
Bogdanov Anton <kortirso@gmail.com>
2023-09-14 20:47:50 +0300

    updat edeploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/log', '
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+set :keep_releases, 2
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
