0bdd44120b85063dc920e554aebdde4a1f51ad7d
Bogdanov Anton <kortirso@gmail.com>
2023-10-16 22:13:38 +0300

    change que log level

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,7 +82,7 @@ namespace :que do
     on roles(:app) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :bundle, 'exec que ./config/environment.rb'
+          execute :bundle, 'exec que -l error ./config/environment.rb'
         end
       end
     end
