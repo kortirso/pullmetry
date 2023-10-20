0622d130d25671167377022ce211aad81f294fb2
Bogdanov Anton <kortirso@gmail.com>
2023-10-20 11:18:00 +0300

    fix calling notifiers

--- app/notifiers/discord_webhook_notifier.rb
+++ app/notifiers/discord_webhook_notifier.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class DiscordWebhookNotifier < AbstractNotifier::Base
-  include Deps[discord_client: 'api.discord.client']
-
   self.driver = proc do |data|
-    discord_client.send_message(
+    Pullmetry::Container['api.discord.client'].send_message(
       path: data[:path],
       body: data[:body]
     )
--- app/notifiers/slack_webhook_notifier.rb
+++ app/notifiers/slack_webhook_notifier.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class SlackWebhookNotifier < AbstractNotifier::Base
-  include Deps[slack_client: 'api.slack.client']
-
   self.driver = proc do |data|
-    slack_client.send_message(
+    Pullmetry::Container['api.slack.client'].send_message(
       path: data[:path],
       body: data[:body]
     )
