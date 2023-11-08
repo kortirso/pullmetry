9e0aae6cc49834a9abd7f15d14bb1ed824e7565c
Bogdanov Anton <kortirso@gmail.com>
2023-11-08 19:42:49 +0300

    added removing identities

---
+++ app/controllers/identities_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class IdentitiesController < ApplicationController
+  before_action :find_identity, only: %i[destroy]
+  before_action :check_identity_count, only: %i[destroy]
+
+  def destroy
+    @identity.destroy
+    redirect_to profile_path
+  end
+
+  private
+
+  def find_identity
+    @identity = current_user.identities.find_by(id: params[:id])
+    redirect_to profile_path, alert: 'Identity is not found' if @identity.nil?
+  end
+
+  def check_identity_count
+    return if current_user.identities.count > 1
+
+    redirect_to profile_path, alert: 'At least 1 identity must exist'
+  end
+end
--- app/javascript/components/Flash/Flash.jsx
+++ app/javascript/components/Flash/Flash.jsx
@@ -7,30 +7,41 @@ export const Flash = ({ values }) => {
     setTimeout(() => setMessages({}), 4000);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const flashBackground = (value) => {
-    if (value === 'notice') return 'bg-green-400';
+  const closeMessage = (key) => setMessages(Object.entries(messages).filter(([elementKey, value]) => elementKey !== key));
 
-    return 'bg-red-400';
-  };
-
-  const closeMessage = (key, value) => {
-    const values = messages[key].filter((element) => element !== value);
-    setMessages({ ...messages, [key]: values });
+  const closeMessageFromList = (key, value) => {
+    const result = messages[key].filter((element) => element !== value);
+    setMessages({ ...messages, [key]: result });
   }
 
   const renderValues = (key, values) => {
-    return values.map((value, index) => (
-      <div
-        className={`relative mb-2 py-4 pl-8 pr-12 rounded shadow ${flashBackground(key)}`}
-        key={`${key}-${index}`}
-      >
-        <p>{value}</p>
-        <span
-          className="absolute top-1 right-1 cursor-pointer p-2"
-          onClick={() => closeMessage(key, value)}
-        >x</span>
-      </div>
-    ))
+    if (Array.isArray(values)) {
+      return values.map((value, index) => (
+        <div
+          className='relative mb-2 py-4 pl-8 pr-12 rounded bg-orange-500'
+          key={`${key}-${index}`}
+        >
+          <p>{value}</p>
+          <span
+            className="absolute top-1 right-1 cursor-pointer p-2"
+            onClick={() => closeMessageFromList(key, value)}
+          >x</span>
+        </div>
+      ))
+    } else {
+      return (
+        <div
+          className='relative mb-2 py-4 pl-8 pr-12 rounded bg-orange-500'
+          key={key}
+        >
+          <p>{values}</p>
+          <span
+            className="absolute top-1 right-1 cursor-pointer p-2"
+            onClick={() => closeMessage(key)}
+          >x</span>
+        </div>
+      )
+    }
   }
 
   return (
