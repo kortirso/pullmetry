08fcd3111d64a6a857569750a3cf2b37fbc6413a
Bogdanov Anton <kortirso@gmail.com>
2024-03-21 15:18:38 +0300

    added statistics at welcome page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- statistics at welcome page
+
 ### Modified
 - repository link validation of ending with .git
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,27 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate
 
+  before_action :find_active_counters, only: %i[index]
+
   def index; end
 
   def privacy; end
 
   def metrics; end
+
+  private
+
+  def find_active_counters
+    @active_counters =
+      Rails.cache.fetch(
+        'welcome_index_active_counters_v1',
+        expires_in: 24.hours,
+        race_condition_ttl: 10.seconds
+      ) do
+        {
+          companies: Company.where.associated(:insights).distinct.count,
+          repositories: Repository.where.associated(:insights).distinct.count
+        }
+      end
+  end
 end
