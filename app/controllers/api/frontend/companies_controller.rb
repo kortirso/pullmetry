616e5753840e69bddde06ce1fe825918f0a6c2d1
Bogdanov Anton <kortirso@gmail.com>
2024-07-01 21:44:03 +0300

    allow to create companies for coowner accounts

--- app/controllers/api/frontend/companies_controller.rb
+++ app/controllers/api/frontend/companies_controller.rb
@@ -6,14 +6,26 @@ module Api
       include Deps[create_form: 'forms.companies.create']
 
       def create
-        # commento: companies.title
-        case create_form.call(user: current_user, params: company_params)
+        # commento: companies.title, companies.user_id
+        case create_form.call(user: user, params: company_params)
         in { errors: errors } then render json: { errors: errors }, status: :ok
         else render json: { redirect_path: companies_path }, status: :ok
         end
       end
 
       private
+
+      def user
+        return current_user if current_user.uuid == account_uuid || account_uuid.nil?
+
+        account = User.find_by!(uuid: account_uuid)
+        current_user.receive_invites.coowner.accepted.write.find_by!(inviteable_id: account.id)
+        account
+      end
+
+      def account_uuid
+        params[:company][:user_uuid]
+      end
 
       def company_params
         params.require(:company).permit(:title)
