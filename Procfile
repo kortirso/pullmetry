9c18be170a2988d630cf41275630d0be2c928e25
Bogdanov Anton <kortirso@gmail.com>
2024-09-04 19:40:47 +0300

    update yarn script

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: RAILS_ENV=development bin/rails server -p 5000
-js: yarn start
+js: yarn build
 css: bin/rails tailwindcss:watch
 background: que -q default -q notifiers
--- esbuild.config.js
+++ esbuild.config.js
@@ -2,19 +2,12 @@ import path from 'path';
 import { context } from 'esbuild';
 import { solidPlugin } from 'esbuild-plugin-solid';
 
-async function build() {
-  let ctx = await context({
-    entryPoints: [path.join(process.cwd(), 'app/javascript/application.jsx')],
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outdir: path.join(process.cwd(), 'app/assets/builds'),
-    absWorkingDir: path.join(process.cwd(), 'app/javascript'),
-    plugins: [solidPlugin()],
-  }).catch(() => process.exit(1))
-
-  await ctx.watch()
-  console.log('watching...')
-}
-
-build();
+context({
+  entryPoints: [path.join(process.cwd(), 'app/javascript/application.jsx')],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outdir: path.join(process.cwd(), 'app/assets/builds'),
+  absWorkingDir: path.join(process.cwd(), 'app/javascript'),
+  plugins: [solidPlugin()],
+}).catch(() => process.exit(1))
