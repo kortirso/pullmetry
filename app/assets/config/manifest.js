c6f1bea4572bfeff8e9f5b081b3435b6b8f5867f
Bogdanov Anton <kortirso@gmail.com>
2024-10-28 13:24:47 +0300

    update emailbutler

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link emailbutler.css
 //= link que/view/application.css
 //= link_tree ../builds
