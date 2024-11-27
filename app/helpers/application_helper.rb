4f3fa81c56e53bdad278eed2c9f7d8be54e6befc
Bogdanov Anton <kortirso@gmail.com>
2024-11-27 11:31:59 +0300

    fix rendering embedded svgs from sprite

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,10 +27,25 @@ module ApplicationHelper
       svg = doc.at_css 'svg'
       svg['class'] = options[:class] if options[:class].present?
     else
-      doc = "<!-- SVG #{filename} not found -->"
+      svg = "<!-- SVG #{filename} not found -->"
     end
 
-    raw doc
+    raw svg
+  end
+
+  def embedded_svg_symbol(filename, symbol_name, options={})
+    asset = Rails.application.assets.find_asset('icon_sprite.svg')
+
+    if asset
+      file = asset.source.force_encoding('UTF-8')
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "symbol##{symbol_name}"
+      svg['class'] = options[:class] if options[:class].present?
+    else
+      svg = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw svg.to_s.gsub('symbol', 'svg')
   end
   # rubocop: enable Rails/OutputSafety
 
