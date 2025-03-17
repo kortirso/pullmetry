15dc05064d376ae17fe83dc32df331e35a5d539c
Bogdanov Anton <kortirso@gmail.com>
2025-03-17 18:55:29 +0300

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,7 +83,7 @@ namespace :que do
     on roles(:app) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute "ps aux | grep '/var/www/html/pullmetry/shared/bundle/ruby/3.2.0/[b]in/que' | awk '{ print $2 }' | xargs kill"
+          execute "ps aux | grep '/var/www/html/pullmetry/shared/bundle/ruby/3.4.1/[b]in/que' | awk '{ print $2 }' | xargs kill"
         end
       end
     end
